903b14071d17f0dd5ee4f8a77e9d7e25842f6a46
Christophe <cavo789@yahoo.com>
2018-11-09 22:39:25 +0100

    Update show_table.php

--- show_table.php
+++ show_table.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Christophe Avonture - https://www.aesecure.com
+ * Author : AVONTURE Christophe - https://www.avonture.be.
+ *
  * Written date  : 2016-10-16
  * Last modified : 2017-10-04
  *
@@ -33,9 +34,9 @@
  *    a limit (limit=10), ...
  *
  * Example : https://youriste/show_table.php?password=Joomla&format=RAW
-  */
+ */
 
-namespace aeSecure;
+namespace Avonture;
 
 define('TITLE', 'Example of Show_Table');
 
@@ -44,28 +45,28 @@ define('TITLE', 'Example of Show_Table');
 // date and the group of the user (registered, super-users, ...)
 
 define(
-	'SQL',
-	'SELECT U.id UserID, U.name Name, U.username UserName, '.
-	'U.email eMail, U.registerDate RegisterDate, '.
-	'U.lastvisitDate LastVisitDate, G.title GroupTitle '.
-	'FROM `#__users` U '.
-	'LEFT JOIN (`#__user_usergroup_map` as UG) ON UG.user_id=U.id '.
-	'LEFT JOIN (`#__usergroups` as G) on UG.group_id=G.id '.
-	'ORDER BY registerDate DESC, name, GroupTitle ASC'
+    'SQL',
+    'SELECT U.id UserID, U.name Name, U.username UserName, ' .
+    'U.email eMail, U.registerDate RegisterDate, ' .
+    'U.lastvisitDate LastVisitDate, G.title GroupTitle ' .
+    'FROM `#__users` U ' .
+    'LEFT JOIN (`#__user_usergroup_map` as UG) ON UG.user_id=U.id ' .
+    'LEFT JOIN (`#__usergroups` as G) on UG.group_id=G.id ' .
+    'ORDER BY registerDate DESC, name, GroupTitle ASC'
 );
 
 // SQL statement for retrieving informations from, f.i., the content table
 /*
 define(
-	'SQL',
-	'SELECT C.id As Article_ID, C.title As Article_Title, '.
-	'G.title As Category_Title, '.
-	'U.name As Author_Name, C.Hits As Hits, C.language As Language, '.
-	'C.created As Writen_Date '.
-	'FROM `#__content` C LEFT JOIN `#__categories` G ON C.catid = G.id '.
-	'LEFT JOIN `#__users` U on C.created_by=U.id '.
-	'WHERE (state=1) '.
-	'ORDER BY C.created DESC'
+    'SQL',
+    'SELECT C.id As Article_ID, C.title As Article_Title, '.
+    'G.title As Category_Title, '.
+    'U.name As Author_Name, C.Hits As Hits, C.language As Language, '.
+    'C.created As Writen_Date '.
+    'FROM `#__content` C LEFT JOIN `#__categories` G ON C.catid = G.id '.
+    'LEFT JOIN `#__users` U on C.created_by=U.id '.
+    'WHERE (state=1) '.
+    'ORDER BY C.created DESC'
 );
 */
 define('DEBUG', false);
@@ -87,278 +88,279 @@ define('PASSWORD', '57ac91865e5064f231cf620988223590');
 
 class ShowTable
 {
-	private static $format = '';
+    private static $format = '';
 
-	public function __construct()
-	{
-		if (DEBUG===true) {
-			ini_set("display_errors", "1");
-			ini_set("display_startup_errors", "1");
-			ini_set("html_errors", "1");
-			ini_set("docref_root", "http://www.php.net/");
-			ini_set("error_prepend_string", "<div style='color:red; font-family:verdana; border:1px solid red; padding:5px;'>");
-			ini_set("error_append_string", "</div>");
-			error_reporting(E_ALL);
-		} else {
-			ini_set('error_reporting', E_ALL & ~ E_NOTICE);
-		}
+    public function __construct()
+    {
+        if (DEBUG === true) {
+            ini_set('display_errors', '1');
+            ini_set('display_startup_errors', '1');
+            ini_set('html_errors', '1');
+            ini_set('docref_root', 'http://www.php.net/');
+            ini_set('error_prepend_string', "<div style='color:red; font-family:verdana; border:1px solid red; padding:5px;'>");
+            ini_set('error_append_string', '</div>');
+            error_reporting(E_ALL);
+        } else {
+            ini_set('error_reporting', E_ALL & ~E_NOTICE);
+        }
 
-		// Die if the pasword isn't supplied
-		self::checkPassword();
+        // Die if the pasword isn't supplied
+        self::checkPassword();
 
-		// Die if no configuration.php file found
-		self::checkConfiguration();
+        // Die if no configuration.php file found
+        self::checkConfiguration();
 
-		// Load Joomla framework
-		self::loadConfiguration();
+        // Load Joomla framework
+        self::loadConfiguration();
 
-		// Get the requested format : HTML or RAW.
-		// If nothing is specified, HTML will be the default one
-		static::$format = filter_input(INPUT_GET, 'format', FILTER_SANITIZE_STRING);
-		static::$format = strtoupper(static::$format);
-		if (!in_array(static::$format, array('HTML','RAW'))) {
-			static::$format='HTML';
-		}
+        // Get the requested format : HTML or RAW.
+        // If nothing is specified, HTML will be the default one
+        static::$format = filter_input(INPUT_GET, 'format', FILTER_SANITIZE_STRING);
+        static::$format = strtoupper(static::$format);
+        if (!in_array(static::$format, ['HTML', 'RAW'])) {
+            static::$format='HTML';
+        }
 
-		$RAW=(static::$format==='RAW');
+        $RAW=('RAW' === static::$format);
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	  * Check if the password is valid; if not, stop immediatly
-	 */
-	private function checkPassword()
-	{
-		// Get the password from the query string
-		$password=filter_input(INPUT_GET, 'password', FILTER_SANITIZE_STRING);
+    /**
+     * Run the query and return the recordset.
+     */
+    public function getRows()
+    {
+        $rows = [];
 
-		if (md5($password) !== PASSWORD) {
-			header('HTTP/1.0 403 Forbidden');
-			echo '<form action="'.$_SERVER['PHP_SELF'].'" method="GET">Password: <input type="text" name="password" /><input class="Submit" type="submit" name="submit" /></form>';
-			die();
-		}
-	}
+        try {
+            $db = \JFactory::getDBO();
+            $db->setQuery(SQL);
 
-	/**
-	 * Die if no configuration.php file found
-	 */
-	private function checkConfiguration()
-	{
-		if (!file_exists($config = rtrim(ROOT, DS).DS.'configuration.php')) {
-			die('<strong>The file '.$config.' can\'t be found, please review the ROOT constant to match your website root folder</strong>');
-		}
-		return true;
-	}
+            $rows = $db->loadObjectList();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
 
-	/**
-	* Load Joomla framework
-	*/
-	private function loadConfiguration()
-	{
-		if (!defined('_JEXEC')) {
-			define('_JEXEC', 1);
-		}
+        return $rows;
+    }
 
-		if (!defined('JPATH_BASE')) {
-			define('JPATH_BASE', rtrim(ROOT, DS));
-		}
+    /**
+     * Add CSS to the page.
+     */
+    public function addCSS()
+    {
+        $script = '';
+        if ('HTML' === static::$format) {
+            $arr=[
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css',
+                'https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css',
+            ];
 
-		if (!defined('JPATH_PLATFORM')) {
-			define('JPATH_PLATFORM', rtrim(ROOT, DS).DS.'libraries');
-		}
+            foreach ($arr as $style) {
+                $script .= "<link rel='stylesheet' href='" . $style . "' " .
+                    "rel='stylesheet' media='screen' />\n";
+            }
+        }
 
-		// include joomla core files (disable errors because
-		// Joomla produde WARNINGs and NOTICES)
-		$error=error_reporting();
-		error_reporting(0);
+        return $script . "\n";
+    }
 
-		if (file_exists($fname = JPATH_BASE.'/includes/defines.php')) {
-			require_once($fname);
-		}
+    /**
+     * Add JS to the page.
+     */
+    public function addJS()
+    {
+        $script = '';
 
-		if (file_exists($fname = JPATH_BASE.'/includes/framework.php')) {
-			require_once($fname);
-		}
+        if ('HTML' === static::$format) {
+            $arr=[
+                '//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
+                '//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js',
+                '//cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js',
+                '//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js',
+                '//cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js',
+                '//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js',
+                '//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js',
+                //'//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js',
+                //'//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js'
+            ];
 
-		if (file_exists($fname = JPATH_BASE.'/includes/application.php')) {
-			require_once($fname);       // No more present since J3.2
-		}
+            foreach ($arr as $js) {
+                $script .= "<script type='text/javascript' src='" . $js . "'></script>\n";
+            }
 
-		if (file_exists($fname = JPATH_BASE.'/libraries/joomla/factory.php')) {
-			require_once($fname);
-		}
+            // Initialize scripts
+            $script .= "<script type='text/javascript'>\n" .
+                "$(document).ready(function () {\n" .
+                "	// Setup - add a text input to each footer cell\n" .
+                "	$('#tbl tfoot th').each( function () {\n" .
+                "		$(this).html('<input type=\"text\" placeholder=\"Search\" />');\n" .
+                "	});\n" .
+                "	\n" .
+                "	$('#tbl').DataTable({\n" .
+                "		'fixedHeader': false,\n" .
+                "		'scrollY': '60vh',\n" .
+                "		'scrollX': '100%',\n" .
+                "		'scrollCollapse': true,\n" .
+                "		'info': true,\n" .
+                "   	'fixedHeader': true,\n" .
+                "   	'dom' : 'Bfrtip',\n" .
+                "   	'buttons' : ['copy', 'csv', 'excel', 'print'], \n" .
+                "		'lengthMenu': [ \n" .
+                "			[25, 50, 100, 500, -1], \n" .
+                "			[25, 50, 100, 500, 'All'] \n" .
+                "		] \n" .
+                "	});\n" .
+                "	\n" .
+                "	// Apply the search\n" .
+                "   var tbl = $('#tbl').DataTable();\n" .
+                "	tbl.columns().every(function(){\n" .
+                "		var that = this;\n" .
+                "		$('input', this.footer()).on('keyup change', function(){\n" .
+                "			if (that.search() !== this.value) {\n" .
+                "				that.search(this.value).draw();\n" .
+                "			}\n" .
+                "		});\n" .
+                "	});\n" .
+                "});\n" .
+                '</script>';
+        }
 
-		if (file_exists($fname = JPATH_BASE.'/libraries/joomla/log/log.php')) {
-			require_once($fname);
-		}
+        return $script;
+    }
 
-		error_reporting($error);
+    public function outputTable()
+    {
+        $rows = self::getRows();
 
-		require_once(JPATH_BASE.'/configuration.php');
+        $return = '';
 
-		return true;
-	}
+        if (count($rows) > 0) {
+            // Output the table
+            $table = '<table id="tbl" class="display compact nowrap order-column">';
 
-	/**
-	 * Run the query and return the recordset
-	 */
-	public function getRows()
-	{
-		$rows = array();
+            // Output the list of fields name
+            $line='';
 
-		try {
-			$db = \JFactory::getDBO();
-			$db->setQuery(SQL);
+            foreach ($rows[0] as $field => $value) {
+                $line .= '<th>' . $field . '</th>';
+            }
 
-			$rows = $db->loadObjectList();
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
+            $table .=
+                '<thead><tr>' . $line . '</tr></thead>' .
+                '<tfoot><tr>' . $line . '</tr></tfoot>' .
+                '<tbody>';
 
-		return $rows;
-	}
+            foreach ($rows as $row) {
+                $line='';
+                foreach ($row as $field => $value) {
+                    $line .= '<td>' . $value . '</td>';
+                }
+                $table .= '<tr>' . $line . '</tr>';
+            } // foreach
 
-	/**
-	 * Add CSS to the page
-	 */
-	public function addCSS()
-	{
-		$script = "";
-		if (static::$format==='HTML') {
-			$arr=array(
-				'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-				'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css',
-				'https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css'
-			);
+            $table .= '</tbody></table>';
+        } // if (count($rows)>0)
 
-			foreach ($arr as $style) {
-				$script .= "<link rel='stylesheet' href='".$style."' ".
-					"rel='stylesheet' media='screen' />\n";
-			}
-		}
+        $return = $table;
 
-		return $script."\n";
-	}
+        if ('HTML' === static::$format) {
+            // Get a few informations
+            $infos = '<p><strong>Number of records &nbsp;:&nbsp;' .
+                number_format(count($rows)) . '</strong></p>';
 
-	/**
-	 * Add JS to the page
-	 */
-	public function addJS()
-	{
-		$script = "";
+            $sTitle = trim(TITLE);
+            if ('' !== $sTitle) {
+                $sTitle = '<h1>' . $sTitle . '</h1>';
+            }
 
-		if (static::$format==='HTML') {
-			$arr=array(
-				'//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
-				'//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
-				'//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js',
-				'//cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js',
-				'//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js',
-				'//cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js',
-				'//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js',
-				'//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js'
-				//'//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js',
-				//'//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js'
-			);
+            $return = '<div style="margin:10px;">' . $sTitle . $table . $infos . '</div>';
+        }
 
-			foreach ($arr as $js) {
-				$script .=  "<script type='text/javascript' src='".$js."'></script>\n";
-			}
+        return  $return;
+    }
 
-			// Initialize scripts
-			$script .= "<script type='text/javascript'>\n".
-				"$(document).ready(function () {\n".
-				"	// Setup - add a text input to each footer cell\n".
-				"	$('#tbl tfoot th').each( function () {\n".
-				"		$(this).html('<input type=\"text\" placeholder=\"Search\" />');\n".
-				"	});\n".
-				"	\n".
-				"	$('#tbl').DataTable({\n".
-				"		'fixedHeader': false,\n".
-				"		'scrollY': '60vh',\n".
-				"		'scrollX': '100%',\n".
-				"		'scrollCollapse': true,\n".
-				"		'info': true,\n".
-				"   	'fixedHeader': true,\n".
-				"   	'dom' : 'Bfrtip',\n".
-				"   	'buttons' : ['copy', 'csv', 'excel', 'print'], \n".
-				"		'lengthMenu': [ \n".
-				"			[25, 50, 100, 500, -1], \n".
-				"			[25, 50, 100, 500, 'All'] \n".
-				"		] \n".
-				"	});\n".
-				"	\n".
-				"	// Apply the search\n".
-				"   var tbl = $('#tbl').DataTable();\n".
-				"	tbl.columns().every(function(){\n".
-				"		var that = this;\n".
-				"		$('input', this.footer()).on('keyup change', function(){\n".
-				"			if (that.search() !== this.value) {\n".
-				"				that.search(this.value).draw();\n".
-				"			}\n".
-				"		});\n".
-				"	});\n".
-				"});\n".
-				"</script>";
-		}
+    /**
+     * Check if the password is valid; if not, stop immediatly.
+     */
+    private function checkPassword()
+    {
+        // Get the password from the query string
+        $password=filter_input(INPUT_GET, 'password', FILTER_SANITIZE_STRING);
 
-		return $script;
-	}
+        if (PASSWORD !== md5($password)) {
+            header('HTTP/1.0 403 Forbidden');
+            echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="GET">Password: <input type="text" name="password" /><input class="Submit" type="submit" name="submit" /></form>';
+            die();
+        }
+    }
 
-	public function outputTable()
-	{
-		$rows = self::getRows();
+    /**
+     * Die if no configuration.php file found.
+     */
+    private function checkConfiguration()
+    {
+        if (!file_exists($config = rtrim(ROOT, DS) . DS . 'configuration.php')) {
+            die('<strong>The file ' . $config . ' can\'t be found, please review the ROOT constant to match your website root folder</strong>');
+        }
 
-		$return = '';
+        return true;
+    }
 
-		if (count($rows)>0) {
-			// Output the table
-			$table = '<table id="tbl" class="display compact nowrap order-column">';
+    /**
+     * Load Joomla framework.
+     */
+    private function loadConfiguration()
+    {
+        if (!defined('_JEXEC')) {
+            define('_JEXEC', 1);
+        }
 
-			// Output the list of fields name
-			$line='';
+        if (!defined('JPATH_BASE')) {
+            define('JPATH_BASE', rtrim(ROOT, DS));
+        }
 
-			foreach ($rows[0] as $field => $value) {
-				$line.='<th>'.$field.'</th>';
-			}
+        if (!defined('JPATH_PLATFORM')) {
+            define('JPATH_PLATFORM', rtrim(ROOT, DS) . DS . 'libraries');
+        }
 
-			$table .=
-				'<thead><tr>'.$line.'</tr></thead>'.
-				'<tfoot><tr>'.$line.'</tr></tfoot>'.
-				'<tbody>';
+        // include joomla core files (disable errors because
+        // Joomla produde WARNINGs and NOTICES)
+        $error=error_reporting();
+        error_reporting(0);
 
-			foreach ($rows as $row) {
-				$line='';
-				foreach ($row as $field => $value) {
-					$line.='<td>'.$value.'</td>';
-				}
-				$table .= '<tr>'.$line.'</tr>';
-			} // foreach
+        if (file_exists($fname = JPATH_BASE . '/includes/defines.php')) {
+            require_once $fname;
+        }
 
-			$table .= '</tbody></table>';
-		} // if (count($rows)>0)
+        if (file_exists($fname = JPATH_BASE . '/includes/framework.php')) {
+            require_once $fname;
+        }
 
-		$return = $table;
+        if (file_exists($fname = JPATH_BASE . '/includes/application.php')) {
+            require_once $fname;       // No more present since J3.2
+        }
 
-		if (static::$format==='HTML') {
-			// Get a few informations
-			$infos = '<p><strong>Number of records &nbsp;:&nbsp;'.
-				number_format(count($rows)).'</strong></p>';
+        if (file_exists($fname = JPATH_BASE . '/libraries/joomla/factory.php')) {
+            require_once $fname;
+        }
 
-			$sTitle = trim(TITLE);
-			if ($sTitle!=='') {
-				$sTitle = "<h1>".$sTitle."</h1>";
-			}
+        if (file_exists($fname = JPATH_BASE . '/libraries/joomla/log/log.php')) {
+            require_once $fname;
+        }
 
-			$return = '<div style="margin:10px;">'.$sTitle.$table.$infos.'</div>';
-		}
+        error_reporting($error);
 
-		return  $return;
-	}
+        require_once JPATH_BASE . '/configuration.php';
+
+        return true;
+    }
 }
 
-$showTable = new \aeSecure\ShowTable();
+$showTable = new \Avonture\ShowTable();
 
 ?>
 
